72749ac6b20c43c1bb6b0a6f665f4bbb6916e2a8
NC14 <36342727+nc14@users.noreply.github.com>
2018-06-07 20:31:12 +0100

    Initial Commit

---
+++ Fiimpl Finished/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Fiimpl Finished
+//
+//  Created by Cockerill, Nicholas on 07/06/2018.
+//  Copyright © 2018 Fiimpl Fitness. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
