cbcb64ee152a4e16a50054707d87f105dff1b0c5
NC14 <36342727+nc14@users.noreply.github.com>
2018-06-11 17:16:39 +0100

    Model structs are complete.
    Realm added to project and to git

--- Fiimpl Finished/Model/Model.swift
+++ Fiimpl Finished/Model/Model.swift
@@ -11,3 +11,129 @@ import UIKit
 import RealmSwift
 
 let realm = try! Realm()
+
+// MARK: Notes Object
+
+class NotesObject: Object {
+    
+    @objc dynamic var note = ""
+    
+}
+
+// MARK: Streak Objects
+
+class StreakObject: Object {
+    @objc dynamic var lastWorkoutDate = Date()
+    @objc dynamic var currentStreak = 0
+    @objc dynamic var longestStreak = 0
+}
+
+//MARK: Workout Objects
+
+/* An exercise generator object is what provides the criteria for an instance of an exercise.
+ It has a range of reps it could choose from and contains a function to generate a WorkoutExercise instance. */
+
+class ExerciseGeneratorObject: Object {
+    @objc dynamic var name = ""
+    @objc dynamic var minReps = 0
+    @objc dynamic var maxReps = 0
+    
+    override class func primaryKey() -> String? {
+        return "name"
+    }
+    
+    convenience init(name: String, minReps: Int, maxReps: Int) {
+        self.init()
+        self.name = name
+        self.minReps = minReps
+        self.maxReps = maxReps
+    }
+
+    // this function generates an instance of the workout exercise
+    func generateExercise() -> WorkoutExercise {
+        return WorkoutExercise(
+            name: name,
+            reps: Int(arc4random_uniform(UInt32(maxReps-minReps))) + minReps
+        )
+    }
+}
+
+/* A Workout Exercise is a stored instance of an exercise.
+ It is generated from the generator object and forms part of a workout */
+
+class WorkoutExercise: Object {
+    @objc dynamic var name = ""
+    @objc dynamic var reps = 0
+    
+    convenience init(name: String, reps: Int) {
+        self.init()
+        self.name = name
+        self.reps = reps
+    }
+    
+}
+
+/* The workout generator contains the struct and functions to generate a full workout, made up of various Exercise Objects.*/
+
+ struct WorkoutGenerator {
+    
+    // This is the exercise pool it draws from - a pool of exerciseGeneratorObjects.
+    
+    let realmExercisePool = realm.objects(ExerciseGeneratorObject.self)
+    
+    // Min and max amount of exercises in the workout
+    
+    let minCount: Int
+    let maxCount: Int
+    
+    // Function to generate a workout
+    
+    func generate() -> Workout {
+        let amount = Int(arc4random_uniform(UInt32(maxCount - minCount))) + minCount
+        let myExercises : [WorkoutExercise] = (0..<amount).map { _ in
+            // Selects an exercise generator at random
+            let index = Int(arc4random_uniform(UInt32(realmExercisePool.count)))
+            // Generates a random workout exercise from this generator
+            
+            return realmExercisePool[index].generateExercise()
+        }
+        
+        return Workout(workoutExercises: myExercises)
+        
+    }
+
+}
+
+// This is the stored instance of a completed workout session.
+
+class WorkoutSessionObject: Object {
+    @objc dynamic var workoutID = UUID().uuidString
+    @objc dynamic var workoutName = ""
+    let exercises = List<WorkoutExercise>()
+    @objc dynamic var totalExerciseCount = 0
+    @objc dynamic var rounds = 0
+    @objc dynamic var favourite : Bool = false
+    
+    override class func primaryKey() -> String? {
+        return "workoutName"
+    }
+}
+
+// This struct is used to pass time variables and the workout details between VC's.
+
+struct FinalWorkout {
+    
+    var generatedWorkout : Workout
+    var timeForWorkout : Int
+    
+}
+
+// This struct holds workout exercises to pass between VC's.
+
+struct Workout {
+    
+    let workoutExercises : [WorkoutExercise]
+    
+}
+
+
---
+++ Pods/Target Support Files/Pods-Fiimpl Finished/Pods-Fiimpl Finished-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Fiimpl_Finished : NSObject
+@end
+@implementation PodsDummy_Pods_Fiimpl_Finished
+@end
