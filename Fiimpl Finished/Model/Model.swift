3397df22848643d22a67405a62816dd77f8fb5d5
NC14 <36342727+nc14@users.noreply.github.com>
2018-06-11 13:19:11 +0100

    about to install cocoa pods

---
+++ Fiimpl Finished/Model/Model.swift
@@ -0,0 +1,13 @@
+//
+//  Model.swift
+//  Fiimpl Finished
+//
+//  Created by Cockerill, Nicholas on 11/06/2018.
+//  Copyright © 2018 Fiimpl Fitness. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import RealmSwift
+
+let realm = try! Realm()
