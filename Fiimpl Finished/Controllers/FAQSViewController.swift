322778bb3141da20f71b4238158643328174a94f
NC14 <36342727+nc14@users.noreply.github.com>
2018-06-30 18:40:45 +0100

    small UI changes - added new controller to start textview at top for FAQs

---
+++ Fiimpl Finished/Controllers/FAQSViewController.swift
@@ -0,0 +1,43 @@
+//
+//  FAQSViewController.swift
+//  Fiimpl Finished
+//
+//  Created by Cockerill, Nicholas on 30/06/2018.
+//  Copyright © 2018 Fiimpl Fitness. All rights reserved.
+//
+
+import UIKit
+
+class FAQSViewController: UIViewController {
+
+    @IBOutlet weak var textFieldOutlet: UITextView!
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        textFieldOutlet.setContentOffset(CGPoint.zero, animated: false)
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
